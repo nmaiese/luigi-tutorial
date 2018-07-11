2600eeabcf44c63144e4d9d272c75c2712ecbb45
Aniello Maiese <n.maiese@gmail.com>
2018-07-11 16:03:02 +0200

    Adding dependencies and parameters

--- luigitutorial.py
+++ luigitutorial.py
@@ -13,5 +13,22 @@ class HelloWorld(luigi.Task):
         with self.output().open('w') as outfile:
             outfile.write('Hello World!\n')
 
+#  Let's try adding another task, NameSubstituter, that will take the file we created in our HelloWorld task, and replace "World" with some name.
+
+class NameSubstituter(luigi.Task):
+    name = luigi.Parameter()
+
+    def requires(self): 
+        return HelloWorld()
+    def output(self): 
+        return luigi.LocalTarget(self.input().path + '.name_' + self.name)
+    def run(self): 
+        with self.input().open() as infile, self.output().open('w') as outfile:
+            text = infile.read()
+            text = text.replace('World', self.name)
+            outfile.write(text)
+
+
+
 if __name__ == '__main__':
     luigi.run()
